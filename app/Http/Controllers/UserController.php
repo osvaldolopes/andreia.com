868c1e39cb34a4d5f8ba27f2a992d11d92cb0714
Osvaldo Filho <osvaldo_filho@live.com>
2022-06-10 21:11:56 -0300

    primeira rota

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function listUser()
+    {
+        echo "<h1>Listagem do Usuario</h1>";
+    }
+}
