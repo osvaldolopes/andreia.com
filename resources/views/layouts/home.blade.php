d18a46b64e4a2c753e821f45fb32efa47e08070f
bRprPzXS7eCjd2L <osvaldo_filho@live.com>
2024-09-23 00:09:18 -0300

    update server

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -240,7 +240,7 @@
                             alt="..." /></div>
                     <div class="timeline-panel">
                         <div class="timeline-heading">
-                            <h4>2009-2011</h4>
+                            <h4>Pedidos</h4>
                             <h4 class="subheading">Our Humble Beginnings</h4>
                         </div>
                         <div class="timeline-body">
@@ -255,7 +255,7 @@
                             alt="..." /></div>
                     <div class="timeline-panel">
                         <div class="timeline-heading">
-                            <h4>March 2011</h4>
+                            <h4>Separação</h4>
                             <h4 class="subheading">An Agency is Born</h4>
                         </div>
                         <div class="timeline-body">
@@ -270,7 +270,7 @@
                             alt="..." /></div>
                     <div class="timeline-panel">
                         <div class="timeline-heading">
-                            <h4>December 2015</h4>
+                            <h4>Valor do Frete</h4>
                             <h4 class="subheading">Transition to Full Service</h4>
                         </div>
                         <div class="timeline-body">
@@ -285,7 +285,7 @@
                             alt="..." /></div>
                     <div class="timeline-panel">
                         <div class="timeline-heading">
-                            <h4>July 2020</h4>
+                            <h4>Postagem</h4>
                             <h4 class="subheading">Phase Two Expansion</h4>
                         </div>
                         <div class="timeline-body">
